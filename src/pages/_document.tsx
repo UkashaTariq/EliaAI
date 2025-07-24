9a0a22c60caf4076d579a16f69b56041fabf49a5
UkashaTariq <ukashatariq1@gmail.com>
2025-07-25 01:53:52 +0500

    Tanstack Query setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Html, Head, Main, NextScript } from "next/document";
+
+const queryClient = new QueryClient();
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <QueryClientProvider client={queryClient}>
+      <Html lang="en">
+        <Head />
+        <body className="antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </QueryClientProvider>
   );
 }
